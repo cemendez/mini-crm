fc41fc141b7ff8b97f4af6aa6fc914d51f16f32b
Carlos Méndez <mcalderon.carlos>
2025-05-04 07:48:46 -0600

    Creación y configuración de métodos para los controladores de Contacto e Interacción

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\InteractionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -14,14 +16,21 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('contacts', ContactController::class);
+
+    Route::get('/contacts/{contact}/interactions', [InteractionController::class, 'index'])->name('interactions.index');
+    Route::post('/contacts/{contact}/interactions', [InteractionController::class, 'store'])->name('interactions.store');
+    Route::put('/interactions/{interaction}', [InteractionController::class, 'update'])->name('interactions.update');
+    Route::delete('/interactions/{interaction}', [InteractionController::class, 'destroy'])->name('interactions.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
